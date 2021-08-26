e463c984797400f8b241c12eb1aef221e4434178
Tony <40594459+Tony308@users.noreply.github.com>
2021-08-26 17:10:22 +0100

    Ci/cd (#2)
    * Completed required unit tests cases. (#1)
    
    Signed-off-by: Tony Huang <40594459+Tony308@users.noreply.github.com>
    
    * Drafted CI/CD pipelines
    
    Signed-off-by: Tony Huang <40594459+Tony308@users.noreply.github.com>

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,5 +1,16 @@
 const request = require('supertest');
 const app = require('../index.js');
+const fs = require('fs');
+const path = require('path');
+
+var base = "";
+var content = "";
+
+beforeEach(() => {
+    base = fs.readFileSync(path.resolve("template.html"), {encoding: "utf-8"});
+});
+
+afterEach(() => app.close());
 
 describe("when requesting '/about-page'", () => {
 
@@ -8,8 +19,22 @@ describe("when requesting '/about-page'", () => {
         .get("/about-page")
         .then(res => {
             expect(res.status).toBe(200);
-            app.close();
+            
         })
+    })
+
+    it("given content, load correct html", () => {
+
+        content = fs.readFileSync(path.resolve("content/about-page/index.md"), {encoding: "utf-8"});
+        base = base.replace("{{content}}", content);
+
+        return request(app)
+        .get("/about-page")
+        .then(res => {
+            expect(res.status).toBe(200);
+            expect(res.text).toEqual(base);
+
+        })    
     })
 })
 
@@ -19,8 +44,23 @@ describe("When requesting '/blog/june/company-update'", () => {
         .get("/blog/june/company-update")
         .then(res => {
             expect(res.status).toBe(200);
+            
         })
     });
+
+    it("given content, load correct html", () => {
+
+        content = fs.readFileSync(path.resolve("content/blog/june/company-update/index.md"), {encoding: "utf-8"});
+        base = base.replace("{{content}}", content);
+
+        return request(app)
+        .get("/blog/june/company-update")
+        .then(res => {
+            expect(res.status).toBe(200);
+            expect(res.text).toEqual(base);
+
+        })    
+    })
 })
 
 describe("When requesting '/jobs'", () => {
@@ -31,7 +71,23 @@ describe("When requesting '/jobs'", () => {
             expect(res.status).toBe(200);
         })
     })
+
+    it("given content, load correct html", () => {
+
+        content = fs.readFileSync(path.resolve("content/jobs/index.md"), {encoding: "utf-8"});
+        base = base.replace("{{content}}", content);
+
+        return request(app)
+        .get("/jobs")
+        .then(res => {
+            expect(res.status).toBe(200);
+            expect(res.text).toEqual(base);
+
+        })    
+    })
 });
+
+
 describe("When requesting '/valves'", () => {
     it("given no errors then, return 200", () => {
         return request(app)
@@ -39,13 +95,30 @@ describe("When requesting '/valves'", () => {
         .then(res => {
             expect(res.status).toBe(200);
         })
+    });
+
+
+    it("given content, load correct html", () => {
+
+        content = fs.readFileSync(path.resolve("content/valves/index.md"), {encoding: "utf-8"});
+        base = base.replace("{{content}}", content);
+
+        return request(app)
+        .get("/valves")
+        .then(res => {
+            expect(res.status).toBe(200);
+            expect(res.text).toEqual(base);
+
+        })    
     })
 });
 
-it("When requesting a URL that doesn't exist then, return 404", () => {
-    return request(app)
-    .get("/test")
-    .then(res => {
-        expect(res.status).toBe(404);
-    })
+describe("failures", () => {
+    it("When requesting a URL that doesn't exist then, return 404", () => {
+        return request(app)
+        .get("/test")
+        .then(res => {
+            expect(res.status).toBe(404);
+        })
+    });
 })
